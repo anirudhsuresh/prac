d170b09616a18a1b57937c823c7b599b7c3fa846
anirudhsuresh <aniramch@ucdavis.edu>
2023-12-02 18:00:07 -0500

    commit this

--- src/App.jsx
+++ src/App.jsx
@@ -3,53 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
+
+
 function App() {
-  const [count, setCount] = useState([])
-  const [track, setTrack] = useState(1)
-  const url = 'https://jsonplaceholder.typicode.com/users/' + track
 
+  const url = 'https://dummyjson.com/products?limit=100'
 
-  async function handleFetch() {
-    setTrack(track + 1)
-    const res = await fetch(url)
-    const data = await res.json()
-    setCount([...count, data])
+  const [data, setData] = useState([])
+
+  const handFetch = async () => {
+    const req = await fetch(url)
+    const res = await req.json()
+    setData([...data, res])
   }
 
-  function handleClick() {
-    handleFetch()
-
-
-  }
 
 
   useEffect(() => {
-    handleFetch()
+    handFetch()
   }, [])
-
 
 
   return (
     <>
-      <button onClick={() => handleClick()}> Click me man</button>
+      hello
       {
-        count ?
-
-          <ul>
-            {
-              count.map((a) => (
-                <>
-                  <li key={a.id}>{a.name}</li>
-                  <ul>
-                    <li key={a.id}>{a.username}</li>
-                  </ul>
-                </>
-              ))
-            }
-          </ul>
-
-
-          : 'No Data'
+        data.length > 0 && console.log(data)
       }
     </>
   )
